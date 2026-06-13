0f81530de232e879a196f85232c15f1beaeeeb3e
elanoanalo <elanoanalo@gmail.com>
2026-06-13 23:29:41 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,37 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Server is running");
-
 app.MapGet("/elano_95_mail_ru", (string? x, string? y) =>
 {
-    if (!long.TryParse(x, out long a) ||
-        !long.TryParse(y, out long b) ||
-        a <= 0 ||
-        b <= 0)
+    if (!long.TryParse(x, out var a) ||
+        !long.TryParse(y, out var b) ||
+        a <= 0 || b <= 0)
     {
-        return Results.Text("NaN");
+        return "NaN";
     }
 
-    long lcm = CalculateLCM(a, b);
-    return Results.Text(lcm.ToString());
+    long gcd(long a, long b)
+    {
+        while (b != 0)
+        {
+            var t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    long lcm = (a / gcd(a, b)) * b;
+
+    return lcm.ToString();
 });
-
-static long CalculateGCD(long a, long b)
-{
-    while (b != 0)
-    {
-        long temp = b;
-        b = a % b;
-        a = temp;
-    }
-
-    return a;
-}
-
-static long CalculateLCM(long a, long b)
-{
-    return a / CalculateGCD(a, b) * b;
-}
 
 app.Run();
